e3dde319784f03f9adf02ef802bea377c7d7cb40
Trevor Menagh <trev@trevreport.org>
2011-02-10 14:49:59 -0500

    Added timezone fix for test runner

--- bin/run_all_tests.sh
+++ bin/run_all_tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd "`dirname \"$0\"`"
-java -jar ../lib/rhino/js.jar ../lib/envjasmine.js `ls -m ../specs | sed -e s/,//g`
+java -Duser.timezone="US/Eastern" -jar ../lib/rhino/js.jar ../lib/envjasmine.js `ls -m ../specs | sed -e s/,//g`
--- bin/run_test.sh
+++ bin/run_test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd "`dirname \"$0\"`"
-java -jar ../lib/rhino/js.jar ../lib/envjasmine.js ../$1
+java -Duser.timezone="US/Eastern" -jar ../lib/rhino/js.jar ../lib/envjasmine.js ../$1
