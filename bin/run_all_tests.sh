164adabc735d20809546aeda7266cb8e45e8233a
Trevor Menagh <trev@trevreport.org>
2011-02-15 09:36:08 -0500

    Added support for arbitrary file structures in specs

--- bin/run_all_tests.sh
+++ bin/run_all_tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd "`dirname \"$0\"`"
-java -Duser.timezone="US/Eastern" -jar ../lib/rhino/js.jar ../lib/envjasmine.js `ls -m ../specs | sed -e s/,//g`
+java -Duser.timezone="US/Eastern" -jar ../lib/rhino/js.jar ../lib/envjasmine.js `find ../specs -name *.spec.js`
