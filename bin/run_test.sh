b98809872e98dc61f3ff4d7fd2f715b2fbb213f7
Trevor Menagh <trev@trevreport.org>
2011-05-31 10:31:02 -0400

    Added ability to run multiple tests with run_test.sh

--- bin/run_test.sh
+++ bin/run_test.sh
@@ -2,4 +2,5 @@
 
 cd "`dirname \"$0\"`"
 PWD=$(pwd)
-java -Duser.timezone="US/Eastern" -jar "$PWD/../lib/rhino/js.jar" "$PWD/../lib/envjasmine.js" "UNIX" "$PWD/.." "$PWD/../$1"
+SPECS=`for i in $*; do echo -n "$PWD/../$i "; done`
+java -Duser.timezone="US/Eastern" -jar "$PWD/../lib/rhino/js.jar" "$PWD/../lib/envjasmine.js" "UNIX" "$PWD/.." $SPECS
