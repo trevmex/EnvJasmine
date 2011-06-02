e43a0f4f686c6e0f3211de903a460c5d191f5e40
javallone <jeff.avallone@gmail.com>
2011-06-02 19:59:09 -0400

    Moving code to prepend the working directory onto spec paths passed into run_test.sh from the shell script into JS.

--- bin/run_test.sh
+++ bin/run_test.sh
@@ -2,5 +2,4 @@
 
 cd "`dirname \"$0\"`"
 PWD=$(pwd)
-SPECS=`for i in $*; do echo -n "$PWD/../$i "; done`
-java -Duser.timezone="US/Eastern" -jar "$PWD/../lib/rhino/js.jar" "$PWD/../lib/envjasmine.js" "UNIX" "$PWD/.." $SPECS
+java -Duser.timezone="US/Eastern" -jar "$PWD/../lib/rhino/js.jar" "$PWD/../lib/envjasmine.js" "UNIX" "$PWD/.." $*
