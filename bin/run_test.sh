c3b78211065746e0f75121a76e89280daa7ffda7
Trevor Lalish-Menagh <trev@trevreport.org>
2011-12-02 16:18:07 -0500

    Fixed a bug with running tests in directories with spaces in them in UNIX

--- bin/run_test.sh
+++ bin/run_test.sh
@@ -2,4 +2,11 @@
 
 cd "`dirname \"$0\"`"
 PWD=$(pwd)
-java -Duser.timezone="US/Eastern" -jar "$PWD/../lib/rhino/js.jar" "$PWD/../lib/envjasmine.js" --environment="UNIX" --rootDir="$PWD/.." $*
+
+CMD="java -Duser.timezone=\"US/Eastern\" -jar \"$PWD/../lib/rhino/js.jar\" \"$PWD/../lib/envjasmine.js\" --environment=\"UNIX\" --rootDir=\"$PWD/..\" "
+
+for (( i=1 ; i < $#+1 ; i=$i+1 )) do
+    CMD="$CMD \"${!i}\""
+done
+
+eval $CMD
