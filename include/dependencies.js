f93839389105860708f3e7099e6ccb1ba9070539
Trevor Lalish-Menagh <trev@trevreport.org>
2012-01-03 14:21:11 -0500

    Fixed a bug in the dependencies file for the sample. Thanks zdk\!

--- include/dependencies.js
+++ include/dependencies.js
@@ -3,7 +3,7 @@
 // NOTE: Load order does matter.
 
 // This is your main JavaScript directory in your project.
-EnvJasmine.jsDir = EnvJasmine.rootDir + "/../../js"; // TODO: Change this to your project's main js directory.
+EnvJasmine.jsDir = EnvJasmine.rootDir + "/samples/"; // TODO: Change this to your project's main js directory.
 
 EnvJasmine.loadGlobal(EnvJasmine.includeDir + "jquery-1.4.4.js"); // for example, load jquery.
 // TODO: Add your own
