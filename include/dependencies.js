a15ce0f6149dff54022b74854df0c0395045edcc
Craig Blaszczyk <masterjakul@gmail.com>
2012-01-20 12:16:07 +0000

    allow more user control over paths to js file in lib dir

--- include/dependencies.js
+++ include/dependencies.js
@@ -2,6 +2,14 @@
 //
 // NOTE: Load order does matter.
 
+// Load the envjasmine environment
+EnvJasmine.loadGlobal(EnvJasmine.libDir + "envjs/env.rhino.1.2.js");
+EnvJasmine.loadGlobal(EnvJasmine.libDir + "jasmine/jasmine.js");
+EnvJasmine.loadGlobal(EnvJasmine.libDir + "jasmine-ajax/mock-ajax.js");
+EnvJasmine.loadGlobal(EnvJasmine.libDir + "jasmine-ajax/spec-helper.js");
+EnvJasmine.loadGlobal(EnvJasmine.libDir + "jasmine-jquery/jasmine-jquery-1.2.0.js");
+EnvJasmine.loadGlobal(EnvJasmine.libDir + "jasmine-rhino-reporter/jasmine-rhino-reporter.js");
+
 // This is your main JavaScript directory in your project.
 EnvJasmine.jsDir = EnvJasmine.rootDir + "/samples/"; // TODO: Change this to your project's main js directory.
 
