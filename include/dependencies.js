5384b7f2a588b6f3ec00f526e6f8205ddbb3948a
Daniel Freiman <djfreiman@gmail.com>
2013-01-10 14:26:49 -0500

    Adding code coverage reporting with JSCover and Sonar

--- include/dependencies.js
+++ include/dependencies.js
@@ -1,7 +1,6 @@
 // Add any files here that need to be loaded before all tests are run, (e.g. third-party libraries, like jQuery)
 //
 // NOTE: Load order does matter.
-
 // Load the envjasmine environment
 EnvJasmine.loadGlobal(EnvJasmine.libDir + "envjs/env.rhino.1.2.js");
 EnvJasmine.loadGlobal(EnvJasmine.libDir + "jasmine/jasmine.js");
@@ -11,7 +10,11 @@ EnvJasmine.loadGlobal(EnvJasmine.libDir + "jasmine-jquery/jasmine-jquery-1.2.0.j
 EnvJasmine.loadGlobal(EnvJasmine.libDir + "jasmine-rhino-reporter/jasmine-rhino-reporter.js");
 
 // This is your main JavaScript directory in your project.
-EnvJasmine.jsDir = EnvJasmine.rootDir + "/samples/"; // TODO: Change this to your project's main js directory.
+EnvJasmine.jsDir = EnvJasmine.jsDir || EnvJasmine.rootDir + "/samples/"; // TODO: Change this to your project's main js directory.
 
 EnvJasmine.loadGlobal(EnvJasmine.includeDir + "jquery-1.4.4.js"); // for example, load jquery.
 // TODO: Add your own
+
+
+// this will include the code coverage plugin
+// EnvJasmine.loadGlobal(EnvJasmine.libDir + "/jscover/envjasmine-coverage.js");
---
+++ samples/notCoveredDemo.js
@@ -0,0 +1,9 @@
+// Sample demo
+
+if (typeof(NS) === 'undefined' || !NS) {
+    var NS = {};
+}
+
+NS.greeter = function (name) {
+    return 'Hello ' + name + '! Cover Me!';
+};
